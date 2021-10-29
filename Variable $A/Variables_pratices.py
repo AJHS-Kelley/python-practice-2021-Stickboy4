8f500fb0a810c34b178e921ba78282a0d17d7c2e
Donte.G <90473716+Stickboy4@users.noreply.github.com>
2021-10-29 14:40:23 -0400

    version 0.1

--- Variable $A/Variables_pratices.py
+++ Variable $A/Variables_pratices.py
@@ -1,1 +1,8 @@
-#Variables Pratice, Donte Gardner, 10/29/21 2:22pm, version 0,0
+#Variables Pratice, Donte Gardner, 10/29/21 2:38pm, version 0,0
+
+# Three Common Types of Data
+
+lives_remaining = 4 # INTEGERS< any +/- WHOLE number and 0
+forty_time = 4.21 # FLOATING POINT, any +/- number with a decimal
+death_cry = "Curse you, Seth! I will be back for revenge"
+# Line 7 is a STRING -- collection of letters/numbers/other chars.
